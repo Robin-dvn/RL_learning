c440bd15bcd469be6415eca955977d48e5f27c18
Robin Davenne <robin.davenne@grenoble-inp.org>
2024-09-29 20:54:39 +0900

    TD0 fonctionnel

--- src/TD0_RandomWalk.py
+++ src/TD0_RandomWalk.py
@@ -1,22 +1,22 @@
 import gymnasium as gym
 import numpy as np
-
+import gym_custom
 
 policy = lambda x: np.random.randint(2)
 
 def main_TD0(nb_ep):
-    env = gym.make('WalkFive-v0') 
+    env = gym.make('GymWalk-v0',render_mode = "human") 
     v = np.zeros(env.observation_space.n)
     for t in range(nb_ep):
-        s = env.reset()
+        s,info = env.reset()
         terminated,truncated = False,False
         while not (terminated):
             a = policy(s)
-            news,r,terminated,_ = env.step(a)
-            v[s] = v[s] + 0.01*(r+v[news]-v(s))
+            news,r,terminated,_,info = env.step(a)
+            v[s] = v[s] + 0.01*(r+v[news]-v[s])
             s = news
 
     env.close()
     return v
 
-v = main_TD0(10)
+v = main_TD0(5)
